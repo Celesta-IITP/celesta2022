4e60937f3fb31a5475ebd88d81024405ec8a85d0
Aditya Kumar Singh <adityakumarsingh1403@gmail.com>
2022-03-31 12:45:05 +0530

    Bug in form fixed

--- client/src/components/Events/workshopJSON.js
+++ client/src/components/Events/workshopJSON.js
@@ -8,7 +8,7 @@ export const workshops = [
         "desc": "Learn how to analyze and gain skills in Deep Learning effectively from scratch and learn about the invariable future of AI and Automation.",
         "date": "9th April",
         "img": deep_learning_web,
-        "form": "https://forms.gle/4oeVFu8wV19MKi928",
+        "form": "https://docs.google.com/forms/d/e/1FAIpQLSfX6YYpfou2wPDn4huCzyGqq1Le5IFa_iHj53ZWhKRvTS_QuA/viewform",
         "rules": ""
     },
     {
@@ -16,7 +16,7 @@ export const workshops = [
         "desc": "Learn basics of NFTs, blockchain & it's applications effectively from scratch & get a chance to win free NFTs after completion of assignments.",
         "date": "10th April",
         "img": the_ledger_web,
-        "form": "https://forms.gle/z8h55kKA9GKhmrDX6",
+        "form": "https://docs.google.com/forms/d/e/1FAIpQLSd_Fn3DcCARS9kR_awdHr5nu3TlK09MmqHUHQcVNYpBk85KiA/viewform",
         "rules": ""
     },
     {
@@ -24,7 +24,7 @@ export const workshops = [
         "desc": "Learn from scratch how to make a ROS based which can traverse autonomously in complex terrains and implement all learings in Hackathon.",
         "date": "11th- 17th April",
         "img": ros_web,
-        "form": "https://forms.gle/jX372QxucqMJpTa26",
+        "form": "https://docs.google.com/forms/d/e/1FAIpQLSePldiuoBlTbUMX98a4MMm_MXJLwXB2v4gdpYFwtm5uOXde6w/viewform",
         "rules": ""
     }
 ]
