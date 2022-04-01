d373fea80fbc16c6c55aff7f78e1af35eb5779c7
Aditya Kumar Singh <adityakumarsingh1403@gmail.com>
2022-04-02 01:23:19 +0530

    Guest Lectures added

---
+++ client/src/components/Events/guestlecture.js
@@ -0,0 +1,24 @@
+import harren from '../../assets/img/GuestLectures/harren_chelle.png'
+import anil from '../../assets/img/GuestLectures/anil_kakodkar.png'
+import prateek from '../../assets/img/GuestLectures/prateek_kishore.png'
+
+export const guestLectures= [
+    {
+        "name": "Haren Chelle",
+        "desc": "He is a well distinguished alumnus of the Indian Institute of Technology (IIT) Patna & Indian Institute of Management (IIM) Bangalore with extensive experience in Business Strategy, Growth, Client Engagement, and Business Development in New-Age Technology solutions such as Robotic Process Automation, Conversational AI & other AI-based customized offerings. He is an aspiring Entrepreneur with a never-ending passion for working with “technology start-ups” and is a strategic advisor for multiple early-stage start-ups in India.",
+        "date": "2nd April @ 3PM",
+        "img": harren
+    },
+    {
+        "name": "Prateek Kishore",
+        "desc": "Mr. Prateek Kishore joined DRDO at Terminal Ballistics Research Laboratory (TBRL), Chandigarh, as Scientist 'B' in 1991\nFor his technical contributions, he has been awarded with :DRDO Young Scientist Award (2002), DRDO Award for Path Breaking Research / Outstanding Technology Development (2007), DRDO Scientist of the Year Award (2011), Special Award for Strategic Contributions (Year 2009, 2012 and 2016).He is a Fellow of Aeronautical Society of India and Life Member of High Energy Materials Society of India (HEMSI). He is also the Vice Chairman of Chandigarh-Delhi Chapter of HEMSI.",
+        "date": "3rd April @ 12PM",
+        "img": prateek
+    },
+    {
+        "name": "Anil Kakodkar",
+        "desc": "Mr. Kakodkar has been a key contributor to India’s strategic programme.On May 18, 1974, he was one of the chosen few who participated in India's first successful Peaceful Nuclear Explosion Experiment at Pokhran. Later, in May 1998, he played a key role in a series of successful nuclear tests conducted at Pokhran. He has created a roadmap for shaping the third stage of India’s nuclear power programme aimed at tapping the vast energy potential of our thorium resources not only as a source for electricity production but also as a primary source for other forms of energy use.",
+        "date": "5th April @ 5:30PM",
+        "img": anil
+    }
+]
